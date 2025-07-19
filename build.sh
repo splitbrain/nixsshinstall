cc5c570189f5213db23b3425a07339813ccd34bc
Andreas Gohr <andi@splitbrain.org>
2025-07-19 23:18:58 +0200

    envrionment only works with impure

--- build.sh
+++ build.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 set -e
 
-echo "Using $SSH_KEY"
+if [ -z "$SSH_KEY" ]; then
+    echo "ERROR: SSH_KEY environment variable is required but not set"
+    exit 1
+fi
 
-nix build .#nixosConfigurations.machineIso.config.system.build.isoImage
+echo "Using SSH_KEY: ${SSH_KEY}"
+
+# Build with SSH key using --impure to allow environment access
+nix build --impure --expr "
+  let
+    flake = builtins.getFlake (toString ./.);
+    sshKey = builtins.getEnv \"SSH_KEY\";
+  in
+    flake.packages.x86_64-linux.isoWithSshKey sshKey
+"
+
 mkdir -p /iso
 cp result/iso/* /iso
 ls -la /iso/
--- flake.nix
+++ flake.nix
@@ -4,20 +4,23 @@
   };
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.machineIso = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-        ({ pkgs, ... }: {
-          users.users.nixos.openssh.authorizedKeys.keys = 
-            let sshKey = builtins.getEnv "SSH_KEY";
-            in if sshKey != "" then [ sshKey ] else [];
+    # Function to build ISO with SSH key
+    packages.x86_64-linux.isoWithSshKey = sshKey: 
+      if sshKey == "" then
+        throw "SSH key cannot be empty"
+      else
+        (nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            ({ pkgs, ... }: {
+              users.users.nixos.openssh.authorizedKeys.keys = [ sshKey ];
 
-          systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+              systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
 
-          isoImage.squashfsCompression = "gzip -Xcompression-level 1";
-        })
-      ];
-    };
+              isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+            })
+          ];
+        }).config.system.build.isoImage;
   };
 }
